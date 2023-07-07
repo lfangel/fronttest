6cb778f0e19a587ecb0dbdb2931dea2239c4e5c5
lfangel <87074562+lfangel@users.noreply.github.com>
2023-07-07 23:02:51 +0800

    設定主題顏色

--- vite.config.js
+++ vite.config.js
@@ -11,17 +11,31 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-        AutoImport({
+    AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1. 設定elementPlus採用sass主題配色
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          // @use "@/styles/var.scss" as *;
+        `,
+      }
     }
   }
 })
